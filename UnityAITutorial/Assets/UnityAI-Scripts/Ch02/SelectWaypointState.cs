b7bd22cb04ecedb66444884336aeafe6cd01a2ef
최현산 <jiwon3_@naver.com>
2022-11-22 10:36:36 +0900

    221122 UnityAI Class
    FSM Tank travel Tutorial

---
+++ UnityAITutorial/Assets/UnityAI-Scripts/Ch02/SelectWaypointState.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectWaypointState : StateMachineBehaviour {
+	 // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        TankAi tankAi = animator.gameObject.GetComponent<TankAi>();
+        tankAi.SetNextPoint();
+    }
+}
